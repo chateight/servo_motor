5b17c314ef28af40679105d88e180d7739307c88
宇佐美隆一 <80155159+chateight@users.noreply.github.com>
2022-05-31 14:18:08 +0900

    touch or iluminace trigger the action of servo motors

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+basic.show_leds("""
+    . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+""")
+
 def on_forever():
+    basic.show_leds("""
+        . # # # .
+                # . . . #
+                # . . . #
+                # . . . #
+                . # # # .
+    """)
     if input.light_level() > 127:
+        music.play_tone(262, music.beat(BeatFraction.SIXTEENTH))
+        basic.show_leds("""
+            . . . . #
+                        . . . # .
+                        . . # . .
+                        . # . . .
+                        # . . . .
+        """)
         servos.P1.set_angle(0)
         servos.P0.set_angle(0)
         basic.pause(500)
--- main.ts
+++ main.ts
@@ -1,5 +1,28 @@
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
 basic.forever(function () {
-    if (input.lightLevel() > 127) {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    if (input.lightLevel() > 127 || input.logoIsPressed()) {
+        music.setVolume(99)
+        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        basic.showLeds(`
+            # . . . #
+            # . . . #
+            # . . . #
+            # . . . #
+            # . . . #
+            `)
         servos.P1.setAngle(0)
         servos.P0.setAngle(0)
         basic.pause(500)
