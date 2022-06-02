843c230a8aed4c885480af3e7e153ad25f735a9e
宇佐美隆一 <80155159+chateight@users.noreply.github.com>
2022-06-02 17:46:58 +0900

    adopt to the real hardware

--- main.ts
+++ main.ts
@@ -6,8 +6,8 @@ basic.forever(function () {
         # . . . #
         . # # # .
         `)
-    if (input.lightLevel() < 3 || (input.logoIsPressed() || input.soundLevel() > 20)) {
-        music.setVolume(99)
+    if (input.lightLevel() < 3 || (input.logoIsPressed() || input.soundLevel() > 50)) {
+        music.setVolume(234)
         music.playTone(262, music.beat(BeatFraction.Sixteenth))
         basic.showLeds(`
             . # . # .
@@ -24,9 +24,9 @@ basic.forever(function () {
             # . . . #
             # . . . #
             `)
-        servos.P0.setAngle(0)
-        basic.pause(100)
         servos.P0.setAngle(150)
-        basic.pause(1000)
+        basic.pause(5000)
+        servos.P0.setAngle(0)
+        basic.pause(200)
     }
 })
